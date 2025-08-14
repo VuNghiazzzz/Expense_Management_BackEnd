5e68072cc0772eaea618eb6782d70f78277ff737
VuTrungNghia <vunghia467@gmail.com>
2025-08-14 21:41:39 +0700

    add Annotation Repo

--- src/main/java/com/example/Expense/management/repository/TransactionRepository.java
+++ src/main/java/com/example/Expense/management/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.Expense.management.repository;
 
 import com.example.Expense.management.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository <Transaction, Long> {
 }
