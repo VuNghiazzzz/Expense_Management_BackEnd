2aa9f2e85c763616bd19cc717ee901c979e868da
VuTrungNghia <vunghia467@gmail.com>
2025-08-16 22:00:15 +0700

    Update Service Category, Category Repository

--- src/main/java/com/example/Expense/management/repository/CategoryRepository.java
+++ src/main/java/com/example/Expense/management/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.example.Expense.management.repository;
 
 import com.example.Expense.management.entity.Category;
+import com.example.Expense.management.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository <Category, Long> {
+    List<Category> findByUser(User user);
 }
