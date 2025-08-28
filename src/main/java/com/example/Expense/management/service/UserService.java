6736e7dddf90346748c30b0b6798e6a53ca07671
VuTrungNghia <vunghia467@gmail.com>
2025-08-28 21:33:06 +0700

    Update Login Token and Update JDK java version 21

--- src/main/java/com/example/Expense/management/service/UserService.java
+++ src/main/java/com/example/Expense/management/service/UserService.java
@@ -1,9 +1,9 @@
 package com.example.Expense.management.service;
 
 import com.example.Expense.management.dto.UserDto;
-import com.example.Expense.management.entity.LoginDto;
+import com.example.Expense.management.dto.LoginDto;
 import com.example.Expense.management.entity.User;
-import com.example.Expense.management.loginreponse.LoginMesage;
+import com.example.Expense.management.loginreponse.LoginMessage;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +11,6 @@ public interface UserService {
     UserDto registerUser(UserDto userDto);
     User findByUsername(String username);
     User getCurrentUser();
-    LoginMesage LoginUser(LoginDto loginDto);
+    LoginMessage LoginUser(LoginDto loginDto);
 
 }
